4a08e240669e175dc416df94e0a6a954c495cf9f
Konstantin <chuikoff.ru@gmail.com>
2017-01-25 20:38:46 +1200

    Выполнил задание 2В
    Правильный вывод всех видов инициалов

--- src/index.js
+++ src/index.js
@@ -5,12 +5,41 @@ const app = express();
 
 app.use(cors()); 
 
+//Задание 2А + Б
+
 app.get('/', (req, res) => {
 
     const a = parseInt(req.query.a) || 0;
     const b = parseInt(req.query.b) || 0;
 
     res.send(`${a+b}`);
+});
+
+//Задание 2В
+
+app.get('/task2', (req, res) => {
+    let fullname = req.query.fullname || '';
+    fullname = fullname.replace(/[0-9]/g, '');
+    const preresult = fullname.split(' ');
+    let result = '';
+
+    if(fullname.length > 0) {
+        if(preresult.length == 3) {
+            result = `${preresult[2]} ${preresult[0].charAt(0)}. ${preresult[1].charAt(0)}.`;
+        } else if(preresult.length == 2) {
+            result = `${preresult[1]} ${preresult[0].charAt(0)}.`;
+        } else if(preresult.length == 1) {
+            result = `${preresult[0]}`;
+        } else if(preresult.length == 0 || preresult.length > 3) {
+            result = 'Invalid fullname';
+        }
+    } else {
+        result = 'Invalid fullname';
+    }
+
+    
+
+    res.send(result);
 });
 
 app.listen(3000, () => {
