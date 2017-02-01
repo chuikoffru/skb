7f5423bbfd773e50c945f337323e4d20293232cd
Konstantin <chuikoff.ru@gmail.com>
2017-02-01 13:52:34 +1200

    Выполнил задание 2D

--- src/index.js
+++ src/index.js
@@ -48,12 +48,43 @@ app.get('/task2c', (req, res) => {
     let url = req.query.username || '';
 
     if(url.length > 0) {
+        //https://regex101.com/r/MQAzW2/1
         result = url.replace(/(((http(s|)\:\/\/)|(\/\/)|(www\.)|\@)|(.*)\/)|(\?.+)/gi, '');
         result = `@${result}`;
     } else {
         result = 'Invalid username'
     }
     res.send(`${result}`);
+});
+
+// Задание 2D
+
+app.get('/task2d', (req, res) => {
+    let result = '';
+    console.log('Зашло ' ,req.query.color);
+    if(req.query.color) {
+        let color = req.query.color.toLowerCase().replace(/(#|%23| )/, '') || '';
+        const revalid = color.match(/[a-fA-F0-9]/g);
+        console.log('Преобразовалось', color);
+        if(color && color.length == revalid.length) {
+            if(color.length > 6 || color.length < 3 || (color.length > 3 && color.length < 6)) {
+                result = 'Invalid color';
+            } else if(color.length == 6) {
+                result = `#${color}`;
+            } else if(color.length == 3) {
+                result += '#';
+                for(let i=0; i < color.length; i++) {
+                    result += color[i].repeat(2);
+                }
+            }
+        } else {
+            result = 'Invalid color';
+        }
+    } else {
+        result = 'Invalid color';
+    }
+
+    res.send(result);
 });
 
 app.listen(3000, () => {
